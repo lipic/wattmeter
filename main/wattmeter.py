1edd4cc2a4a7b5f8fb4c2889d378cafd3be6998e
lipic <lipapave@seznam.cz>
2020-04-05 19:35:27 +0200

    Predelan uart 1 na softUART

--- main/wattmeter.py
+++ main/wattmeter.py
@@ -1,12 +1,11 @@
 from main import modbus
-from machine import UART
+import machine
 import time
 class Wattmeter:
      
     def __init__(self):
         
-        self.uart = UART(2,115200)
-        self.uart.init(115200,bits=8, parity=None, stop=1)
+        self.uart = machine.UART(1, baudrate=115200, rx=26, tx=27)
         self.modbusClient = modbus.Modbus()
         self.rmsLayer = RMSlayer()
 
@@ -20,12 +19,12 @@ class Wattmeter:
             time.sleep(0.05)
             #self.datalayer.data["E1"] =     (int)((((receiveData[5])) << 24) | ((receiveData[6])<< 16) | (((receiveData[3])) << 8) | ((receiveData[4])))
     
-            self.rmsLayer.data["I1"] =     (int)((((receiveData[4])) << 8)  | ((receiveData[3])))
-            self.rmsLayer.data["I2"] =     (int)((((receiveData[6])) << 8)  | ((receiveData[5])))
-            self.rmsLayer.data["I3"] =     (int)((((receiveData[8])) << 8)  | ((receiveData[7])))
-            self.rmsLayer.data["U1"] =     (int)((((receiveData[10])) << 8)  | ((receiveData[9])))
-            self.rmsLayer.data["U2"] =     (int)((((receiveData[12])) << 8) | ((receiveData[11])))
-            self.rmsLayer.data["U3"] =     (int)((((receiveData[14])) << 8) | ((receiveData[13])))
+            self.rmsLayer.data["I1"] =     (int)((((receiveData[3])) << 8)  | ((receiveData[4])))
+            self.rmsLayer.data["I2"] =     (int)((((receiveData[5])) << 8)  | ((receiveData[6])))
+            self.rmsLayer.data["I3"] =     (int)((((receiveData[7])) << 8)  | ((receiveData[8])))
+            self.rmsLayer.data["U1"] =     (int)((((receiveData[9])) << 8)  | ((receiveData[10])))
+            self.rmsLayer.data["U2"] =     (int)((((receiveData[11])) << 8) | ((receiveData[12])))
+            self.rmsLayer.data["U3"] =     (int)((((receiveData[13])) << 8) | ((receiveData[14])))
             return None
 
         except Exception as e:
