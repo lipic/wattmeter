db1a31c8ddd63c1a62249472c9d3fc541978e823
lipic <lipapave@seznam.cz>
2020-05-22 17:30:34 +0200

    rev_0.756

--- main/wattmeter.py
+++ main/wattmeter.py
@@ -44,7 +44,7 @@ class Wattmeter:
                 self.dataLayer.data["E1"] =     (float)(((self.receiveData[5]) << 24) | ((self.receiveData[6])<< 16) | (((self.receiveData[3])) << 8) | ((self.receiveData[4])))
                 self.dataLayer.data["E2"] =     (float)(((self.receiveData[9]) << 24) | ((self.receiveData[10])<< 16) | (((self.receiveData[7])) << 8) | ((self.receiveData[8])))
                 self.dataLayer.data["E3"] =     (float)(((self.receiveData[13]) << 24) | ((self.receiveData[14])<< 16) | (((self.receiveData[11])) << 8) | ((self.receiveData[12])))
-                return "{}".format(self.receiveData[3],self.receiveData[4],self.receiveData[5],self.receiveData[6])
+                return "{};{};{};{}".format(self.receiveData[3],self.receiveData[4],self.receiveData[5],self.receiveData[6])
             
             elif (self.receiveData and (reg == 3000)):
                 
