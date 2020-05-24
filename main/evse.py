84da604b264fe7facaf011f40cea8a28517bf6f7
lipic <lipapave@seznam.cz>
2020-05-24 10:20:20 +0200

    rev_0.783

--- main/evse.py
+++ main/evse.py
@@ -38,9 +38,8 @@ class Evse():
         
         try:
             if(self.receiveData):
-                self.receiveData = self.receiveData[1:]
-                    
-                if ((reg == 1000)and (0 == self.modbusClient.mbrtu_data_processing(self.receiveData))):
+               
+                if ((reg == 1000)and (0 == self.modbusClient.mbrtu_data_processing(self.receiveData[1:]))):
                     self.dataLayer.data["EVSE1"] =     (int)((((self.receiveData[3])) << 8)  | ((self.receiveData[4])))
                     self.dataLayer.data["EVSE2"] =     (int)((((self.receiveData[5])) << 8)  | ((self.receiveData[6])))
                     self.dataLayer.data["EVSE3"] =     (int)((((self.receiveData[7])) << 8)  | ((self.receiveData[8])))
