0029f5f32d878bae78eb9c82d8f53e4437ebbff1
lipic <lipapave@seznam.cz>
2020-05-24 10:28:57 +0200

    rev_0.785

--- main/evse.py
+++ main/evse.py
@@ -44,7 +44,7 @@ class Evse():
                     self.dataLayer.data["EVSE1"] =     (int)((((self.receiveData[3])) << 8)  | ((self.receiveData[4])))
                     self.dataLayer.data["EVSE2"] =     (int)((((self.receiveData[5])) << 8)  | ((self.receiveData[6])))
                     self.dataLayer.data["EVSE3"] =     (int)((((self.receiveData[7])) << 8)  | ((self.receiveData[8])))
-                    return "Data from wattmeter address: {} were received. Result: {}".format(reg,result)
+                    return "Data from wattmeter address: {} were received.".format(reg)
                         
                 else: 
                     return "Timed out waiting for result."
